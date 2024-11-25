669486a3539de1fc49683d44484867ff9cac7529
Gaurav Bansode <gauravbansode90@gmail.com>
2024-11-25 10:32:12 -0700

    Add MongoDB Connection in FastAPI

--- fastapi_server/app/main.py
+++ fastapi_server/app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 import psycopg2
 from psycopg2.extras import RealDictCursor
-# import db_connect as db
+from pymongo import MongoClient
 
 
 # db.connect_to_db("scm")
@@ -17,8 +17,12 @@ def get_db_connection():
         port=26257
     )
 
+def get_mongo_connection():
+    client = MongoClient("mongodb://host.docker.internal:27017/", serverSelectionTimeoutMS=5000)
+    return client
+
 # ADD APIS as Needed a post and get api have been created.
-@app.get("/orders")
+@app.get("/customers")
 async def get_orders():
     conn = get_db_connection()
     try:
@@ -39,3 +43,15 @@ async def get_orders(item: dict):
         conn.close()
         # cur.close()
         # conn.close()
+
+@app.post("/orders")
+async def get_orders():
+    client = get_mongo_connection()
+    db = client["scm"]
+    orders_collection = db["orders"]
+    # Fetch orders from MongoDB
+    mongo_orders = list(orders_collection.find({}, {"_id": 0}))
+    response = {
+        "mongo_orders": mongo_orders
+    }
+    return response
